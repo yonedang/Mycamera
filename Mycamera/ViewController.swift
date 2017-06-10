070b477b4a60514b2fbbeb1faa2bcddf0bc498f9
米田 央 <ochang@o-yoneda-macbook.local>
2017-06-10 11:18:38 +0900

    Initial Commit

---
+++ Mycamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mycamera
+//
+//  Created by 米田 央 on 2017/06/10.
+//  Copyright © 2017年 Swift-Yoneda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
